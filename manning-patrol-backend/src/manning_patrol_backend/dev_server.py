82f4484a1ab62129b2146c9d480daaca2e032e40
Jakob <jdj1994@gmail.com>
2026-09-23 11:56:30 +0200

    re-added missing backend folder to align with .toml setup

--- manning-patrol-backend/src/manning_patrol_backend/dev_server.py
+++ manning-patrol-backend/src/manning_patrol_backend/dev_server.py
@@ -5,7 +5,7 @@ import uvicorn
 def main():
     """Run uvicorn with reload."""
     uvicorn.run(
-        "manning_patrol_backend:app",
+        "manning_patrol_backend.positioning_simulator:app",
         host="0.0.0.0",
         port=8000,
         reload=True,
